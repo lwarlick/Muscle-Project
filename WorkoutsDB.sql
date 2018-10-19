ed168b2bb7336c81f1b6d8ba65025a845c58acac
lwarlick <42449890+lwarlick@users.noreply.github.com>
2018-10-19 14:00:06 -0500

    Delete WorkoutsDB.sql

--- WorkoutsDB.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE workouts;
-CREATE TABLE allWorkouts {
-	name TEXT;
-	length INTEGER;
-	musclesWorked TEXT;
-}
-INSERT INTO allWorkouts(name, length, musclesWorked) VALUES ('Bench Press', 5, 'Pectoralis, Triceps');
